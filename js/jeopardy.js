0e40d254ab4a3b52731845f17c2cbf84bc2a1269
jiara <ji.arakani@gmail.com>
2022-06-17 15:41:39 -0400

    Register click events

--- js/jeopardy.js
+++ js/jeopardy.js
@@ -1,27 +1,42 @@
 const numRows = 5
 const numColumns = 5
+let categoryArray = []
+// Select an integer out of 18418 integers representing each Jeopardy category
+const randomInt = () => Math.floor((Math.random() * 18418) + 1)
 
 // Initialize gameboard categories + clues on page load
-initCategories()
-
-initClues()
+document.querySelector('button').addEventListener('click', initCategories)
 
 function initCategories() {
     let catRow = document.getElementById("category-row")
+    // Delete existing categories
+    if (catRow) {
+        categoryArray = []
+        while (catRow.firstChild) {
+            catRow.removeChild(catRow.firstChild);
+        }
+    }
+    // Create category boxes
     for (let b = 0; b < numColumns; b++) {
         let box = document.createElement('div')
         box.className = 'category-box clue-box'
         catRow.appendChild(box)
     }
+    initClues()
 }
+
 function initClues() {
     let board = document.getElementById("gameboard")
-    // POPULATE 5 ROWS...
+    // Delete existing clues
+    let clueRows = document.querySelectorAll('.clue-row')
+    clueRows.forEach(row => row.remove())
+
+    // Create a number of rows...
     for (let r = 0; r < numRows; r++) {
         let row = document.createElement('div')
         let boxValue = 200 * (r + 1)
         row.className = 'clue-row'
-        //  ... OF 5 BOXES
+        //  ... with a number of boxes
         for (let b = 0; b < numColumns; b++) {
             let box = document.createElement('div')
             box.className = 'clue-box'
@@ -32,78 +47,59 @@ function initClues() {
         }
         board.appendChild(row)
     }
+    buildCategories()
 }
-
-// Select an integer out of 18418 integers representing each Jeopardy category
-const randomInt = () => Math.floor((Math.random() * 18418) + 1)
 
 function buildCategories() {
     // url jservice.io to query category ids 
     // -> TO DO: Save to localstorage rather than simply a global variable
     // -> TO DO: Ensure each random integers is unique
-    
     let requestArray = []
     
     for (let r = 1; r <= numColumns; r++) {
         this["fetchReq" + r] = fetch(`https://jservice.io/api/category?&id=${randomInt()}`).then(res => res.json())
         requestArray.push(this["fetchReq" + r])
     }
-    // console.log(requestArray)
+
     // Collect all data once all requests are fulfilled
     const allData = Promise.all(requestArray)
 
-    allData.then(res => console.log(res))
+    allData.then(res => {
+        console.log(res)
+        categoryArray = res
+        setCategories(categoryArray)
+        // setClues(categoryArray)
+    })
 }
 
-function getClue() {
-    console.log(this.textContent)
+function setCategories(categoryArray) {
+    let element = document.getElementById('category-row')
+    let children = element.children
+    for (let i = 0; i < children.length; i++) {
+        children[i].innerHTML = categoryArray[i].title
+    }
 }
 
-// class GameBoard {
-//     constructor(gameData) {
-//         this._ = 
-//         this._ = 
-//         this._ = 
-//     }
-//     testCall() {
+// Get selected box
+function getClue(event) {
+    let child = event.currentTarget
+    child.classList.add('selected-box')
+    // Store value of box
+    let boxValue = child.innerHTML.slice(1)
+    // Find box index with parent
+    let parent = child.parentNode
+    // What tf is this .call() method?
+    let index = Array.prototype.findIndex.call(parent.children, (c) => c === child)
+    // Get array of clues from categoryArray for the relevant category (at box index)
+    let cluesList = categoryArray[index].clues
+    // Find first clue object from array that is worth value of box
+    let clue = cluesList.find(obj => {
+        return obj.value == boxValue
+    })
+    
+    displayQuestion(clue, child, boxValue)
+}
 
-//     }
-//     toDOM() {
+// function displayQuestion() {
 
-//     }
-// }
-
-// function enterInput() {
-
-// }
-
-// function assessInput() {
-
-// }
-
-// function changeDisplay() {
-
-// }
-
-// function populate() {
-//     const url = ``
-    // const fetchReq1 = fetch(`https://jservice.io/api/category?&id=${randomInt()}`).then(res => res.json)
-    // const fetchReq2 = fetch(`https://jservice.io/api/category?&id=${randomInt()}`).then(res => res.json)
-    // const fetchReq3 = fetch(`https://jservice.io/api/category?&id=${randomInt()}`).then(res => res.json)
-    // const fetchReq4 = fetch(`https://jservice.io/api/category?&id=${randomInt()}`).then(res => res.json)
-    // const fetchReq5 = fetch(`https://jservice.io/api/category?&id=${randomInt()}`).then(res => res.json)
-    // requestArray.push(fetchReq1)
-    // requestArray.push(fetchReq2)
-    // requestArray.push(fetchReq3)
-    // requestArray.push(fetchReq4)
-    // requestArray.push(fetchReq5)
-
-//     fetch(url)
-//         .then(res => res.json)
-//         .then(data => {
-//             console.log(data)
-//         })
-//         .catch(err => {
-//             console.log(`error! ${err}`)
-//         })
 // }
